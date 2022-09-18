888d1f6f5fb7ec08d3ab106d230565aa1a905ee4
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 01:03:00 +0300

    Six

---
+++ Six/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Lessons
+{
+    internal class Program
+    {
+        static void foo(ref int[] myArray, int newSize)
+        {
+            int[] newArray = new int[newSize]; // создание нового масива для добавления елемента
+            for (int i = 0; i < myArray.Length && i < newSize; i++) // цикл для копирования данных со старого масива в новый
+                newArray[i] = myArray[i]; // копирование данных масива
+            myArray = newArray; // измена ссылки масива
+            for (int i = 0; i < newSize; i++) // Заполнения елементов массива если увеличение массива
+            {
+                if (newArray[i] >= i)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"Введите {i}-й елемент");
+                    newArray[i] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] myArray = new int[6]; // Создание массива 
+            for (int i = 0; i < myArray.Length; i++) // Заполнение массива
+            {
+                myArray[i] = random.Next(100);
+            }
+            Console.Write("Введите размер нового массива : ");
+            int newSize = int.Parse(Console.ReadLine()); // рaзмер нового масива 
+            foo(ref myArray, newSize); // вызов метода 
+            for (int i = 0; i < myArray.Length; i++) // вывод нашего масива
+            {
+                Console.WriteLine(myArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Six/obj/Debug/net5.0/Six.AssemblyInfo.cs
+++ Six/obj/Debug/net5.0/Six.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Five")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Six")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Five")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Five")]
+[assembly: System.Reflection.AssemblyProductAttribute("Six")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Six")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
